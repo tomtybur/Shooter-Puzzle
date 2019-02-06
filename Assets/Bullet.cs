d2f14bd83708b47ad8615ca71c5138c41873c0db
Tom Tyburczy <tyburczt@gmail.com>
2019-02-06 14:24:22 -0700

    Added Instance Gun project from class

---
+++ Assets/Bullet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public float lifespan = 1.5f;
+    private float counter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        counter = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        counter += Time.deltaTime;
+        //transform.Translate(transform.forward.x * speed, transform.forward.y * speed, transform.forward.z * speed);
+        transform.Translate(transform.forward * speed * Time.deltaTime);
+
+        if (counter >= lifespan) 
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Gun.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public Transform Ammo;
+    public float fireDelay = 0.25f;
+    private float counter;
+    private bool canShoot = true;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        counter += Time.deltaTime;
+       if (Input.GetAxis("Fire1") > 0 && canShoot) 
+       {
+            //Instantiate returns the object that gets created
+            Transform Bullet = Instantiate(Ammo, transform.position, Quaternion.identity);
+            Bullet.forward = this.transform.forward;
+            canShoot = false;
+            counter = 0f;
+       }
+       if (counter >= fireDelay) 
+       {
+            canShoot = true;
+       }
+    }
+}
